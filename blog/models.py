cc70540ac412a797e5ca5b73e19e3b324d1d1b07
ravion223 <tablettaoson2016@gmail.com>
2024-02-11 18:30:29 +0200

    added images to posts and design to web pages

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add = True)
+    post_image = models.ImageField(null = True, blank = True, upload_to = "images/")
 
     def __str__(self):
         return self.title
